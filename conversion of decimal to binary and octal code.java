f57aed99cc6dd4a531fc8ffe008db071ab015f46
yokeshTC <112368654+yokeshTC@users.noreply.github.com>
2023-06-10 18:41:46 +0530

    Add files via upload

---
+++ conversion of decimal to binary and octal code.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.Scanner;
+public class compare {
+
+	public static void main(String[] args) 
+	{ Scanner in = new Scanner(System.in); 
+	// decimal number
+	int decimal = in.nextInt(); 
+	// convert decimal to binary
+	String binary = Integer.toBinaryString(decimal); 
+	
+	System.out.println("BINARY IS " + binary); 
+	//convert decimal to octal
+	System.out.print("OCTAL IS "); 
+	System.out.println(Integer.toOctalString(decimal));
+	//assertEqual("1100" , binary);
+	assertTrue(14== decimal);
+	
+	 }
+}
